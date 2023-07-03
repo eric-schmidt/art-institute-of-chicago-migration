2cad9d7894c1797b87978f717590a2b08baa7edd
Eric Schmidt <eric.schmidt@contentful.com>
2023-07-03 10:56:05 -0600

    Adds ability to generate a report of orphan content from a space by content type.

--- src/lib/filesystem.js
+++ src/lib/filesystem.js
@@ -29,3 +29,35 @@ export const listDir = async (path) => {
     console.log(chalk.red(`Error reading file directory: ${error.message}`));
   }
 };
+
+export const createCSV = async (csvData, csvFilename) => {
+  // Check if "public" dir already exists. If not, create it.
+  try {
+    await fs.access("../../public");
+  } catch (error) {
+    // If an error is thrown, the directory doesn't exist
+    if (error.code === "ENOENT") {
+      try {
+        // Create the directory
+        await fs.mkdir("../../public");
+        console.log(chalk.green("Public directory created."));
+      } catch (error) {
+        console.error(chalk.red("Error creating Public directory."), error);
+      }
+    } else {
+      console.error(chalk.red("Error accessing directory."), error);
+    }
+  }
+
+  // Write CSV content to file
+  try {
+    await fs.writeFile(`../../public/${csvFilename}`, csvData.join(","));
+    console.log(
+      chalk.green(
+        `CSV file has been successfully created: /public/${csvFilename}`
+      )
+    );
+  } catch (error) {
+    console.log(chalk.red("Error writing CSV file:"), error);
+  }
+};
---
+++ src/updates/generate-orphan-report.js
@@ -0,0 +1,60 @@
+import chalk, { Chalk } from "chalk";
+import { PromisePool } from "@supercharge/promise-pool";
+import { client } from "../lib/cdaClient.js";
+import { getArguments } from "../lib/commands.js";
+import { createCSV } from "../lib/filesystem.js";
+
+// Object to hold candidates for deletion. Includes a header since this
+// will be exported to CSV later on.
+const orphans = ["Orphan Entry Links"];
+
+const logOrphans = async (type) => {
+  let entries = await client.getEntries({
+    content_type: type,
+    limit: 10,
+    skip: 0,
+  });
+
+  // Get the total number of pages so that we can iterate over all items.
+  const totalPages = Math.ceil(entries.total / entries.limit);
+  const itemsPerPage = 100;
+  for (let i = 0; i <= totalPages; i++) {
+    entries = await client.getEntries({
+      content_type: type,
+      limit: itemsPerPage,
+      skip: itemsPerPage * i,
+    });
+
+    // Run in batches using PromisePool to avoid excessive rate limit errors.
+    // CONCURRENCY is the number of concurrent Promises to run using the Promise Pool.
+    // If you are seeing a lot of rate limit errors, try decreasing this number.
+    const CONCURRENCY = 10;
+    await PromisePool.withConcurrency(CONCURRENCY)
+      .for(entries.items)
+      .process(async (entry) => {
+        const parentEntries = await client.getEntries({
+          links_to_entry: entry.sys.id,
+        });
+        // If the total of items referencing this entry is 0 is is a deletion candidate.
+        if (parentEntries.total === 0) {
+          orphans.push(
+            `https://app.contentful.com/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/${process.env.CONTENTFUL_ENVIRONMENT_ID}/entries/${entry.sys.id}`
+          );
+        }
+      });
+  }
+
+  createCSV(orphans, `${type}Orphans.csv`);
+};
+
+(async function () {
+  const { type } = getArguments();
+
+  if (type) {
+    logOrphans(type);
+  } else {
+    console.log(
+      chalk.red('Please specify a content type. E.g. "--type=imageWrapper".')
+    );
+  }
+})();
