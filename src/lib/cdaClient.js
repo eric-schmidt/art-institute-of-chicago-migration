dbf43c2bdf6dd29695d2f53e70006d61f1c4e261
Eric Schmidt <eric.schmidt@contentful.com>
2023-08-01 09:21:27 -0600

    Removes await from cdaClient, attempting to alleviate bug.

--- src/lib/cdaClient.js
+++ src/lib/cdaClient.js
@@ -5,7 +5,7 @@ import contentful from "contentful";
 dotenv.config();
 
 // Init Contentful delivery client.
-export const client = await new contentful.createClient({
+export const client = contentful.createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
   environment: process.env.CONTENTFUL_ENVIRONMENT_ID,
   accessToken: process.env.CONTENTFUL_CDA_TOKEN,
